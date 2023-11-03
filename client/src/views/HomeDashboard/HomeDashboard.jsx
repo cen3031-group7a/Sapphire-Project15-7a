7268a408d3f859d73fca0ab5da728fd4364d2af1
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-03 14:48:14 -0400

    added navbar and styling to homedashboard

--- client/src/views/HomeDashboard/HomeDashboard.jsx
+++ client/src/views/HomeDashboard/HomeDashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
 
 const buttonStyle = {
   flex: 1,
@@ -31,78 +32,87 @@ export default function HomeDashboard() {
   const navigate = useNavigate();
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-    }}>
-      <h1 style={{
-        textAlign: 'center',
-        fontSize: '48px',
-      }}>Welcome to the Student Homepage</h1>
-      <div style={{ flex: 1, display: 'flex' }}>
-        <Link to="/myprograms" style={{
-          ...buttonStyle,
-          ...pastelYellow,
-          margin: '20px',
-          borderRadius: '20px',
-          border: 'black 2px solid'
+
+    <div className='container nav-padding'>
+      <NavBar />
+      <div id='activity-container'>
+        <div id='header'>
+          <div>Dashboard</div>
+        </div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100vh',
         }}>
-          <button style={{
-            width: '100%',
-            fontSize: '24px',
-            background: 'none',
-            border: 'none',
-            fontSize: '40px',
-          }}>My Programs</button>
-        </Link>
-        <Link to="/parentalcontrols" style={{
-          ...buttonStyle,
-          ...pastelGreen,
-          margin: '20px',
-          borderRadius: '20px',
-          border: 'black 2px solid'
-        }}>
-          <button style={{
-            width: '100%',
-            fontSize: '24px',
-            background: 'none',
-            border: 'none',
-            fontSize: '40px',
-          }}>Parental Controls</button>
-        </Link>
-      </div>
-      <div style={{ flex: 1, display: 'flex' }}>
-        <Link to="/assignments" style={{
-          ...buttonStyle,
-          ...pastelBlue,
-          margin: '20px',
-          borderRadius: '20px',
-          border: 'black 2px solid'
-        }}>
-          <button style={{
-            width: '100%',
-            fontSize: '24px',
-            background: 'none',
-            border: 'none',
-            fontSize: '40px',
-          }}>Classroom</button>
-        </Link>
-        <Link to="/gallery" style={{
-          ...buttonStyle,
-          ...pastePurple,
-          margin: '20px',
-          borderRadius: '20px',
-          border: 'black 2px solid'
-        }}>
-          <button style={{
-            width: '100%',
-            fontSize: '24px',
-            background: 'none',
-            border: 'none',
-            fontSize: '40px',
-          }}>Gallery</button>
-        </Link>
+          <h1 style={{
+            textAlign: 'center',
+            fontSize: '48px',
+          }}>Welcome to the Student Homepage</h1>
+          <div style={{ flex: 1, display: 'flex' }}>
+            <Link to="/myprograms" style={{
+              ...buttonStyle,
+              ...pastelYellow,
+              margin: '20px',
+              borderRadius: '20px',
+              border: 'black 2px solid'
+            }}>
+              <button style={{
+                width: '100%',
+                fontSize: '24px',
+                background: 'none',
+                border: 'none',
+                fontSize: '40px',
+              }}>My Programs</button>
+            </Link>
+            <Link to="/parentalcontrols" style={{
+              ...buttonStyle,
+              ...pastelGreen,
+              margin: '20px',
+              borderRadius: '20px',
+              border: 'black 2px solid'
+            }}>
+              <button style={{
+                width: '100%',
+                fontSize: '24px',
+                background: 'none',
+                border: 'none',
+                fontSize: '40px',
+              }}>Parental Controls</button>
+            </Link>
+          </div>
+          <div style={{ flex: 1, display: 'flex' }}>
+            <Link to="/assignments" style={{
+              ...buttonStyle,
+              ...pastelBlue,
+              margin: '20px',
+              borderRadius: '20px',
+              border: 'black 2px solid'
+            }}>
+              <button style={{
+                width: '100%',
+                fontSize: '24px',
+                background: 'none',
+                border: 'none',
+                fontSize: '40px',
+              }}>Classroom</button>
+            </Link>
+            <Link to="/gallery" style={{
+              ...buttonStyle,
+              ...pastePurple,
+              margin: '20px',
+              borderRadius: '20px',
+              border: 'black 2px solid'
+            }}>
+              <button style={{
+                width: '100%',
+                fontSize: '24px',
+                background: 'none',
+                border: 'none',
+                fontSize: '40px',
+              }}>Gallery</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
