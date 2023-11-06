34aad04ec05389ea01a2b1fa23dc66895a322a42
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-06 11:49:26 -0500

    added styling to the MyPrograms page

--- client/src/views/MyPrograms/MyPrograms.jsx
+++ client/src/views/MyPrograms/MyPrograms.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import './MyPrograms.less';
 
-const MyPrograms= () => {
-    const buttonStyle = {
-        color: 'Black',
-        margin: '30px',
-        textAlign: 'left',
-        padding: '10px',
-        borderRadius: '5px',
-      };
-    const listStyle = {
-        textAlign: 'left',
-        padding: '10px',
-        marginLeft: '50px',
-        marginBottom: '50px',
-        borderRadius: '5px',
-      };
+const MyPrograms = () => {
+  const [menuVisible, setMenuVisible] = React.useState(false);
+  const [selectedProgram, setSelectedProgram] = React.useState(null);
+
+  const toggleMenu = (programId) => {
+    setMenuVisible(!menuVisible);
+    setSelectedProgram(programId);
+  };
 
   return (
     <div className='container nav-padding'>
@@ -24,18 +18,90 @@ const MyPrograms= () => {
         <div id='header'>
           <div>My Programs</div>
         </div>
-        <div id='add-button' style={buttonStyle}>
-            <button>Create New</button>
+        <div id='add-button'>
+          <button>Create New</button>
         </div>
-        <div id='program-list' style={listStyle}>
-            <ul>
-                <li id='program-1'>Program 1</li>
-                <li id='program-2'>Program 2</li>
-                <li id='program-3'>Program 3</li>
-            </ul>
+        <div id='program-list'>
+          <ul>
+            <li id='program-1'>
+              Program 1
+              <div className="dots" onClick={() => toggleMenu('program-1')}>
+                •••
+                {menuVisible && selectedProgram == 'program-1' && (
+                  <div className="menu">
+                    <ul>
+                      <li>Edit</li>
+                      <li>Share</li>
+                      <li>Delete</li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li id='program-2'>
+              Program 2
+              <div className="dots" onClick={() => toggleMenu('program-2')}>
+                •••
+                {menuVisible && selectedProgram == 'program-2' && (
+                  <div className="menu">
+                    <ul>
+                      <li>Edit</li>
+                      <li>Share</li>
+                      <li>Delete</li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li id='program-3'>
+              Program 3
+              <div className="dots" onClick={() => toggleMenu('program-3')}>
+                •••
+                {menuVisible && selectedProgram == 'program-3' && (
+                  <div className="menu">
+                    <ul>
+                      <li>Edit</li>
+                      <li>Share</li>
+                      <li>Delete</li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li id='program-4'>
+              Program 4
+              <div className="dots" onClick={() => toggleMenu('program-4')}>
+                •••
+                {menuVisible && selectedProgram == 'program-4' && (
+                  <div className="menu">
+                    <ul>
+                      <li>Edit</li>
+                      <li>Share</li>
+                      <li>Delete</li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li id='program-5'>
+              Program 5
+              <div className="dots" onClick={() => toggleMenu('program-5')}>
+                •••
+                {menuVisible && selectedProgram == 'program-5' && (
+                  <div className="menu">
+                    <ul>
+                      <li>Edit</li>
+                      <li>Share</li>
+                      <li>Delete</li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
