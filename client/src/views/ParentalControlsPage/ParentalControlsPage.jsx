7bd83a26316cf0efb327e3f5f2e4c6c0457540e9
Tyler Landtroop <tlandtroop@ufl.edu>
2023-11-27 12:02:08 -0500

    added students programs to parental controls page

--- client/src/views/ParentalControlsPage/ParentalControlsPage.jsx
+++ client/src/views/ParentalControlsPage/ParentalControlsPage.jsx
@@ -2,12 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 
-import ToggleSwitch from "../../components/ToggleSwitch/ToggleSwitch"; 
+import ToggleSwitch from "../../components/ToggleSwitch/ToggleSwitch";
 import './ParentalControlsPage.css';
 import GradesComponent from '../Grades/Grades';
-import { updatePermissions, getPermissions } from '../../Utils/requests';
+import { updatePermissions, getPermissions, getPrograms } from '../../Utils/requests';
 
 export default function ParentalControlsPage() {
+  const [canChange, setCanChange] = useState(false);
+  const [programs, setPrograms] = useState([]);
+
+  useEffect(() => {
+    const fetchPrograms = async () => {
+      const { data, err } = await getPrograms();
+      if (!err) {
+        setPrograms(data);
+      } else {
+        console.error('Error fetching programs:', err);
+      }
+    };
+
+    fetchPrograms();
+  }, [])
+
   const listStyle = {
     textAlign: 'left',
     justifyContent: 'center',
@@ -16,26 +32,27 @@ export default function ParentalControlsPage() {
     marginRight: '0px',
     marginBottom: '40px',
     borderRadius: '5px',
+
     //backgroundColor: 'rgba(61, 159, 230, 0.45)',
   }
 
   const columnStyle = {
-    flex: 1, 
-    margin: '10px', 
-    padding: '10px', 
+    flex: 1,
+    margin: '10px',
+    padding: '10px',
     border: '3px solid #3b719f',
     borderRadius: '10px',
     backgroundColor: 'rgba(61, 159, 230, 0.45)',
-    minHeight: '620px',
-  };  
+    minHeight: '34rem',
+  };
 
   const buttonStyle = {
-    display: 'flex', 
+    display: 'flex',
     justifyContent: 'center',
     marginRight: '30px',
   }
 
-  const [canChange, setCanChange] = useState(false);
+
   const handleClick = (e) => {
     setCanChange(!canChange);
     //if canChange is now false (just clicked on save) then update the student's permissions statuses
@@ -53,28 +70,28 @@ export default function ParentalControlsPage() {
   }
 
   //const [isChecked, setChecked] = useState(false); 
-  const {permissionone} = getPermissions("permissionone_viewcoursematerials");
+  const { permissionone } = getPermissions("permissionone_viewcoursematerials");
   console.log(permissionone);
   const [isCheckedViewMaterials, setCheckedViewMaterials] = useState(false); //replaced automatic falses with actual getPermissions status
   //const [isCheckedViewMaterials, setCheckedViewMaterials] = useState(permissionone); //replaced automatic falses with actual getPermissions status
 
-  const {permissiontwo} = getPermissions("permissiontwo_submitlateassignments");
+  const { permissiontwo } = getPermissions("permissiontwo_submitlateassignments");
   const [isCheckedSubmitAssignments, setCheckedSubmitAssignments] = useState(false);
   //const [isCheckedSubmitAssignments, setCheckedSubmitAssignments] = useState(permissiontwo);
 
-  const {permissionthree} = getPermissions("permissionthree_discussionparticipation");
+  const { permissionthree } = getPermissions("permissionthree_discussionparticipation");
   const [isCheckedParticipateDiscussions, setCheckedParticipateDiscussions] = useState(false);
   //const [isCheckedParticipateDiscussions, setCheckedParticipateDiscussions] = useState(permissionthree);
 
-  const {permissionfour} = getPermissions("permissionfour_timelimits");
+  const { permissionfour } = getPermissions("permissionfour_timelimits");
   const [isCheckedToggleTimeLimits, setCheckedToggleTimeLimits] = useState(false);
   //const [isCheckedToggleTimeLimits, setCheckedToggleTimeLimits] = useState(permissionfour);
 
-  const {permissionfive} = getPermissions("permissionfive_accessresources");
+  const { permissionfive } = getPermissions("permissionfive_accessresources");
   const [isCheckedAccessResources, setCheckedAccessResources] = useState(false);
   //const [isCheckedAccessResources, setCheckedAccessResources] = useState(permissionfive);
 
-  const {permissionsix} = getPermissions("permissionsix_receivenotifications");
+  const { permissionsix } = getPermissions("permissionsix_receivenotifications");
   const [isCheckedReceiveNotifications, setCheckedReceiveNotifications] = useState(false);
   //const [isCheckedReceiveNotifications, setCheckedReceiveNotifications] = useState(permissionsix);
 
@@ -91,63 +108,65 @@ export default function ParentalControlsPage() {
           <div style={columnStyle}>
             <h2>Student Projects</h2>
             <div id='program-list' style={listStyle}>
-            <ul>
-                <li id='program-1'>Program 1</li>
-                <li id='program-2'>Program 2</li>
-                <li id='program-3'>Program 3</li>
-            </ul>
-          </div>
+              <ul>
+                {programs.map((program) => (
+                  <Link className='link' to={`/sandbox`} key={program.id}>
+                    <li id={program.id}>{program.title}</li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
           </div>
 
           {/* Student Grades Section */}
-          <div style={columnStyle} id='grades-programs-container'> 
+          <div style={columnStyle} id='grades-programs-container'>
 
-              {renderPerformance()}
-            </div>
+            {renderPerformance()}
+          </div>
 
           {/* Student Permissions Section */}
           <div style={columnStyle}>
             <h2>Student Permissions</h2>
-              <ToggleSwitch 
-                label="View Course Materials"
-                isChecked={isCheckedViewMaterials}
-                onChange={() => {if(canChange) {setCheckedViewMaterials(!isCheckedViewMaterials);}}}
-                disabled={!canChange}
-                />
-              <ToggleSwitch 
-                label="Submit late assignments"
-                isChecked={isCheckedSubmitAssignments}
-                onChange={() => {if(canChange) {setCheckedSubmitAssignments(!isCheckedSubmitAssignments);}}}
-                disabled={!canChange}/>
-              <ToggleSwitch 
-                label="Participate in public discussions"
-                isChecked={isCheckedParticipateDiscussions}
-                onChange={() => {if(canChange) {setCheckedParticipateDiscussions(!isCheckedParticipateDiscussions);}}}
-                disabled={!canChange}/>
-              <ToggleSwitch 
-                label="Toggle time limits"
-                isChecked={isCheckedToggleTimeLimits}
-                onChange={() => {if(canChange) {setCheckedToggleTimeLimits(!isCheckedToggleTimeLimits);}}}
-                disabled={!canChange}/>
-              <ToggleSwitch 
-                label="Access outside resources"
-                isChecked={isCheckedAccessResources}
-                onChange={() => {if(canChange) {setCheckedAccessResources(!isCheckedAccessResources);}}}
-                disabled={!canChange}/>
-              <ToggleSwitch 
-                label="Receive notifications"
-                isChecked={isCheckedReceiveNotifications}
-                onChange={() => {if(canChange) {setCheckedReceiveNotifications(!isCheckedReceiveNotifications);}}}
-                disabled={!canChange}/>
+            <ToggleSwitch
+              label="View Course Materials"
+              isChecked={isCheckedViewMaterials}
+              onChange={() => { if (canChange) { setCheckedViewMaterials(!isCheckedViewMaterials); } }}
+              disabled={!canChange}
+            />
+            <ToggleSwitch
+              label="Submit late assignments"
+              isChecked={isCheckedSubmitAssignments}
+              onChange={() => { if (canChange) { setCheckedSubmitAssignments(!isCheckedSubmitAssignments); } }}
+              disabled={!canChange} />
+            <ToggleSwitch
+              label="Participate in public discussions"
+              isChecked={isCheckedParticipateDiscussions}
+              onChange={() => { if (canChange) { setCheckedParticipateDiscussions(!isCheckedParticipateDiscussions); } }}
+              disabled={!canChange} />
+            <ToggleSwitch
+              label="Toggle time limits"
+              isChecked={isCheckedToggleTimeLimits}
+              onChange={() => { if (canChange) { setCheckedToggleTimeLimits(!isCheckedToggleTimeLimits); } }}
+              disabled={!canChange} />
+            <ToggleSwitch
+              label="Access outside resources"
+              isChecked={isCheckedAccessResources}
+              onChange={() => { if (canChange) { setCheckedAccessResources(!isCheckedAccessResources); } }}
+              disabled={!canChange} />
+            <ToggleSwitch
+              label="Receive notifications"
+              isChecked={isCheckedReceiveNotifications}
+              onChange={() => { if (canChange) { setCheckedReceiveNotifications(!isCheckedReceiveNotifications); } }}
+              disabled={!canChange} />
             <div id='add-button' style={buttonStyle}>
               <button onClick={handleClick}>
                 {canChange ? "Save Permissions" : "Edit Permissions"}
-              </button> 
+              </button>
               {/**/}
             </div>
           </div>
         </div>
-        </div>
-    </div>      
+      </div>
+    </div>
   );
-  };
+};
